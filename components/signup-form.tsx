e7cf6af854a02d8ab76147cd197be3984d4cd5dd
Fadhlullah Mustapha <fadhmusty@gmail.com>
2025-11-28 19:05:07 +0100

    auth revamped

--- components/signup-form.tsx
+++ components/signup-form.tsx
@@ -128,7 +128,7 @@ export function SignupForm({
             <img
               src="/expense1.jpg"
               alt="Signup illustration"
-              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+              className="absolute inset-0 h-full w-full object-cover"
             />
           </div>
         </CardContent>
